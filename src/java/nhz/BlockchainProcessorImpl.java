24bddc50b662675bdbf2ef76a442d319fb989d21
MaWo <ccmawo@gmail.com>
2015-03-31 01:30:20 +0200

    added checkpoints during blockchain download checks known blockIds every 720 blocks

--- src/java/nhz/BlockchainProcessorImpl.java
+++ src/java/nhz/BlockchainProcessorImpl.java
@@ -492,6 +492,16 @@ final class BlockchainProcessorImpl implements BlockchainProcessor {
                 }
 
                 if (! Constants.isTestnet) {
+                	
+                    if (previousLastBlock.getHeight() % 720 == 0 && previousLastBlock.getHeight()<=(CheckPoints.previousBlockId.length-1)*720) {
+                    	int i = previousLastBlock.getHeight()/720;
+                		if (! ((new BigInteger(CheckPoints.previousBlockId[i])).longValue() == previousLastBlock.getId())) {
+                			throw new BlockNotAcceptedException("BlockId check failed at block height "+previousLastBlock.getHeight()+". Expected "+ CheckPoints.previousBlockId[i] +" but found "+Convert.toUnsignedLong(previousLastBlock.getId()));
+                		} else {
+                			Logger.logDebugMessage("Checkpoint passed at block height "+previousLastBlock.getHeight());
+                		}                	
+                    }
+                    
 	                if (previousLastBlock.getHeight() == Constants.TRANSPARENT_FORGING_BLOCK) {
 	                    byte[] checksum = calculateTransactionsChecksum();
 	                    if (CHECKSUM_TRANSPARENT_FORGING == null) {
