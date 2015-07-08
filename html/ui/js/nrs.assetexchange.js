eb34d0e8cd5ab5bca5ff069f2b73f82534200b5c
Michael Wolf <michaelpwolf@gmail.com>
2015-07-08 15:34:09 +0200

    toggle sidebar in asset exchange to make it usable on mobile

--- html/ui/js/nrs.assetexchange.js
+++ html/ui/js/nrs.assetexchange.js
@@ -2043,3 +2043,16 @@ var NRS = (function(NRS, $, undefined) {
 
 	return NRS;
 }(NRS || {}, jQuery));
+
+var assetsidebar = true;
+$("#toggle_asset_exchange_sidebar").click(function(e){
+	e.preventDefault();
+	$("#content-spliter-sidebar_asset_exchange").toggle();
+	if(assetsidebar === true) {
+		$("#content-spliter-right_asset_exchange").css('left','0px');
+		assetsidebar = false;
+	} else if(assetsidebar === false) {
+		$("#content-spliter-right_asset_exchange").css('left','200px');
+		assetsidebar = true;
+	}
+});
