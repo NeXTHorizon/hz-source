809caddd3709ae17efa76e5e843ffb0cbc42e90a
MaWo <ccmawo@gmail.com>
2015-08-26 03:53:02 +0200

    testnet DGS, MS, VS and Phasing block height set to 110000

--- src/java/nxt/Constants.java
+++ src/java/nxt/Constants.java
@@ -133,10 +133,10 @@ public final class Constants {
     public static final int REFERENCED_TRANSACTION_FULL_HASH_BLOCK_TIMESTAMP = Integer.MAX_VALUE;
     public static final int MAX_REFERENCED_TRANSACTION_TIMESPAN = 60 * 1440 * 60;
     public static final int TRANSACTIONS_VERSION_1_BLOCK =  (isTestnet ? 580 : 275000);
-    public static final int DIGITAL_GOODS_STORE_BLOCK =  (isTestnet ? 105000 : Integer.MAX_VALUE); // dgs transactions are disabled
-    public static final int MONETARY_SYSTEM_BLOCK = (isTestnet ? 105000 : Integer.MAX_VALUE);
-    public static final int VOTING_SYSTEM_BLOCK = isTestnet ? 220000 : Integer.MAX_VALUE;
-    public static final int PHASING_BLOCK = isTestnet ? 220000 : Integer.MAX_VALUE;
+    public static final int DIGITAL_GOODS_STORE_BLOCK =  (isTestnet ? 110000 : Integer.MAX_VALUE);
+    public static final int MONETARY_SYSTEM_BLOCK = (isTestnet ? 110000 : Integer.MAX_VALUE);
+    public static final int VOTING_SYSTEM_BLOCK = isTestnet ? 110000 : Integer.MAX_VALUE;
+    public static final int PHASING_BLOCK = isTestnet ? 110000 : Integer.MAX_VALUE;
 
     public static final int LAST_KNOWN_BLOCK = (CheckPoints.previousBlockId.length-1)*720;
     
