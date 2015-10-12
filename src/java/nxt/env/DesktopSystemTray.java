0806d1e00b09268805ff4d74ea74410379f30640
MaWo <ccmawo@gmail.com>
2015-10-13 01:40:22 +0200

    websockets disabled by default

--- src/java/nxt/env/DesktopSystemTray.java
+++ src/java/nxt/env/DesktopSystemTray.java
@@ -182,7 +182,7 @@ public class DesktopSystemTray {
         addDataRow(statusPanel, "Updated", dateFormat.format(new Date()));
         if (statusDialog == null || !statusDialog.isVisible()) {
             JOptionPane pane = new JOptionPane(statusPanel, JOptionPane.PLAIN_MESSAGE, JOptionPane.DEFAULT_OPTION, imageIcon);
-            statusDialog = pane.createDialog(wrapper, "NXT Server Status");
+            statusDialog = pane.createDialog(wrapper, "Horizon Server Status");
             statusDialog.setVisible(true);
             statusDialog.dispose();
         } else {
